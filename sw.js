e7584439b45ed655304e23c5b6ace86a69ea5263
Nico Arato <nico.arato@gmail.com>
2020-09-29 22:47:07 -0300

    Devolviendo una petición ante un error en la petición original.

--- sw.js
+++ sw.js
@@ -1,35 +1,16 @@
 self.addEventListener('fetch', event => {
 
-    // if (event.request.url.includes('style.css')) {
-    //     event.respondWith(null)
-    // } else {
+    event.respondWith(
+        fetch(event.request)
+        .then(resp => {
+            if (resp.ok) {
+                return resp;
+            } else {
+                return fetch('img/main.jpg');
+            }
 
-    //     event.respondWith(fetch(event.request))
-    // }
-
-
-
-    // if (event.request.url.includes('style.css')) {
-
-    //     let respuesta = new Response(`
-    //         body {
-    //             background-color: red !important;
-    //             color: pink;
-    //         }
-    //     `, {
-    //         headers: {
-    //             'Content-Type': 'text/css'
-    //         }
-    //     });
-
-    //     event.respondWith(respuesta);
-
-    if (event.request.url.includes('img/main.jpg')) {
-
-        event.respondWith(fetch('img/main-patas-arriba.jpg'));
-
-    }
-
+        })
+    );
 
 
 });
